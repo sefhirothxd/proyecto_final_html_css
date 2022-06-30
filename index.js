e66e9a4ff880df2c5dfee32001cdb9a3d4a48540
bryan vera <bveracachay@gmail.com>
2022-06-30 02:04:50 -0500

    fist commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const cursos = [
+  {
+    id: 1,
+    fecha: '20 de junio',
+    links: [
+      {
+        id: 1,
+        nombre: 'Curso de JavaScript',
+        url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      },
+      {
+        id: 2,
+        nombre: 'Curso de React',
+        url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      },
+    ],
+  },
+];
